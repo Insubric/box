6ded29178870b6b5f70d7a36e08a5c2cb078e348
Andrea Minetti <andrea.minetti@wavein.ch>
2021-07-09 18:41:13 +0200

    Added blank layout

--- client/src/main/scala/ch/wsl/box/client/views/RootView.scala
+++ client/src/main/scala/ch/wsl/box/client/views/RootView.scala
@@ -11,23 +11,34 @@ import io.udash.bootstrap.BootstrapStyles
 import io.udash.core.Presenter
 import scalacss.ScalatagsCss._
 
-case object RootViewPresenter extends ViewFactory[RootState.type]{
+
+case class RootViewModel(layout:String)
+object RootViewModel extends HasModelPropertyCreator[RootViewModel] {
+  implicit val blank: Blank[RootViewModel] =
+    Blank.Simple(RootViewModel(Layouts.std))
+}
+
+case object RootViewPresenter extends ViewFactory[RootState]{
 
 
-  override def create(): (View, Presenter[RootState.type]) = {
+  override def create(): (View, Presenter[RootState]) = {
 
-    (new RootView(),new RootPresenter())
+    val prop = ModelProperty.blank[RootViewModel]
+
+    (new RootView(prop),new RootPresenter(prop))
   }
 }
 
-class RootPresenter() extends Presenter[RootState.type] {
+class RootPresenter(viewModel:ModelProperty[RootViewModel]) extends Presenter[RootState] {
 
 
-  override def handleState(state: RootState.type): Unit = {
+
+  override def handleState(state: RootState): Unit = {
+    viewModel.set(RootViewModel(state.layout))
   }
 }
 
-class RootView() extends ContainerView {
+class RootView(viewModel:ModelProperty[RootViewModel]) extends ContainerView {
   import scalatags.JsDom.all._
   import io.udash.css.CssView._
 
@@ -37,21 +48,28 @@ class RootView() extends ContainerView {
 
 
 
-  private def content = div(BootstrapStyles.containerFluid)(
-    Header.navbar(UI.title),
-    div(ClientConf.style.notificationArea,
-      repeat(Notification.list){ notice =>
-        div(ClientConf.style.notification,bind(notice)).render
+  private def content = produce(viewModel.subProp(_.layout)) {
+      case Layouts.std => {
+        div(BootstrapStyles.containerFluid)(
+          Header.navbar(UI.title),
+          div(ClientConf.style.notificationArea,
+            repeat(Notification.list){ notice =>
+              div(ClientConf.style.notification,bind(notice)).render
+            }
+          ),
+          main(ClientConf.style.fullHeight)(
+            div()(
+              child
+            )
+          ),
+          Footer.template(UI.logo),
+          LoginPopup.render
+        ).render
       }
-    ),
-    main(ClientConf.style.fullHeight)(
-      div()(
-        child
-      )
-    ),
-    Footer.template(UI.logo),
-    LoginPopup.render
-  )
+      case Layouts.blank => div(BootstrapStyles.containerFluid,overflowX.hidden)( child ).render
+    }
+
+
 
   override def getTemplate: Modifier = content
 
