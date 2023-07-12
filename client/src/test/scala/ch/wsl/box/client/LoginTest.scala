a7f708fafb8efa39d49109802a9c5d859ddebd52
Andrea Minetti <andrea.minetti@wavein.ch>
2023-07-12 15:42:35 +0200

    Fixed LoginTest

--- client/src/test/scala/ch/wsl/box/client/LoginTest.scala
+++ client/src/test/scala/ch/wsl/box/client/LoginTest.scala
@@ -9,9 +9,10 @@ import org.scalajs.dom.window
 
 class LoginTest extends TestBase {
 
+  var logged:Option[String] = None
 
   class LoginValues extends Values {
-    override def loggedUser: Option[String] = None
+    override def loggedUser: Option[String] = logged
   }
 
   override def values: Values = new LoginValues
@@ -23,6 +24,7 @@ class LoginTest extends TestBase {
           _ <- services.clientSession.refreshSession()
           _ = {
             assert(!Context.services.clientSession.logged.get)
+            logged = Some("postgres")
           }
           _ <- Context.services.clientSession.login("test","test")
           _ <- waitLoggedIn
