1b36ef5def2553d9ce8c3aee1fb81b04ff0a92e6
Andrea Minetti <andrea.minetti@wavein.ch>
2025-06-02 13:43:18 +0200

    Fixed pdf viewver

--- client/src/main/scala/ch/wsl/box/client/views/components/widget/FileSimpleWidget.scala
+++ client/src/main/scala/ch/wsl/box/client/views/components/widget/FileSimpleWidget.scala
@@ -121,7 +121,8 @@ case class FileSimpleWidget(widgetParams:WidgetParams) extends Widget with HasDa
               headerFactory = None,
               bodyFactory = Some((interceptor) => {
                 if(mime == "application/pdf") {
-                  iframe(attr("src").bind(url.transform(f => "http://localhost:8080/pdf/web/viewer.html?file="+f)), ClientConf.style.fullWidth, ClientConf.style.fullHeight).render
+                  div(Routes.baseUri)
+                  iframe(attr("src").bind(url.transform(f => s"${Routes.baseUri}pdf/web/viewer.html?file="+f)), ClientConf.style.fullWidth, ClientConf.style.fullHeight).render
                 } else {
                   img(nested(src.bind(url))).render
                 }
