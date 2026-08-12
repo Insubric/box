690dd725e0d025f556cc4f7046d7a7d59aa2456d
Andrea Minetti <andrea.minetti@wavein.ch>
2026-08-12 16:01:18 +0200

    Slider fix

--- client/main.js
+++ client/main.js
@@ -10,3 +10,4 @@ import '@fontsource/open-sans/700.css';
 import '@fontsource/open-sans/400-italic.css';
 import '@fontsource/open-sans/600-italic.css';
 import '@fontsource/open-sans/700-italic.css';
+import 'toolcool-range-slider'
